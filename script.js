80d3338224537cbfcbebfc7bf39a51e2b7bf7ac8
Aditya chandel <adityachandel371@gmail.com>
2025-02-19 20:50:49 +0530

    Added scroll reveal animation

---
+++ script.js
@@ -0,0 +1,11 @@
+// Animation
+
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 1500,
+    delay: 200,
+    reset: false
+});
+
+// home section
+sr.reveal('.project-box', { delay: 50, origin: 'left' });
