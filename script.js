815e5da170bd8bacfb1aae46fac447dcc0f88799
Aditya chandel <adityachandel371@gmail.com>
2025-02-23 21:03:12 +0530

    Added back to top button logic

--- script.js
+++ script.js
@@ -7,5 +7,24 @@ const sr = ScrollReveal({
     reset: false
 });
 
-// home section
+// project section
 sr.reveal('.project-box', { delay: 50, origin: 'left' });
+
+// back to top button logic
+
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTop = document.querySelector('.back-to-top button');
+
+    backToTop.addEventListener('click', function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+    // Show button when scrolling down
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 100) {
+            document.querySelector('.back-to-top').style.display = 'block';
+        } else {
+            document.querySelector('.back-to-top').style.display = 'none';
+        }
+    });
+});
